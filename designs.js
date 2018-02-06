161a3c74e833e4cc61b9cdf9eb4b4e44b8355b3e
bobeius <36049044+bobeius@users.noreply.github.com>
2018-02-06 11:14:45 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,7 +4,28 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  const inputHeight = $("#inputHeight").val();
+  const inputWidth = $("#inputWidth").val();
+  const table = $("#pixelCanvas");
+  table.children().remove();
 
-// Your code goes here!
+  for (let x = 0; x < height; r++) {
+    table.append("<tr></tr>");
+
+    for (let y = 0; y < width; c++) {
+      table.children().last().append("<td></td>");
+    }
+  }
+  
+  table.on("click", "td", function() {
+    let color = $("input[type='color']").val();
+    $(this).attr("bgcolor", color);
+  });
+}
+
+$('input[type="submit"]').on('click', function (type){     
+    type.preventDefault();
+    makeGrid();
+});
 
 }
